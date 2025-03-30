aa1e8c3ea87fd12c108a713ed3b4598cdc30946b
阿珏酱 <1638211921@qq.com>
2025-03-30 10:37:06 +0800

    新增畅听VIP接口 修复搜索单曲问题

--- api/module/search.js
+++ api/module/search.js
@@ -1,17 +1,20 @@
 // 搜索
 module.exports = (params, useAxios) => {
   const dataMap = {
-    platform: 'AndroidFilter',
+    // token: '',
+    albumhide: 0,
+    iscorrection: 1,
     keyword: params?.keywords || '',
+    nocollect: 0,
     page: params?.page || 1,
     pagesize: params?.pagesize || 30,
-    category: 1,
+    platform: 'AndroidFilter',
   };
 
   const type = ['special', 'lyric', 'song', 'album', 'author', 'mv'].includes(params.type) ? params.type : 'song';
 
   return useAxios({
-    url: `/${type === 'song' ? 'v2' : 'v1'}/search/${type}`,
+    url: `/${type === 'song' ? 'v3' : 'v1'}/search/${type}`,
     method: 'GET',
     params: dataMap,
     encryptType: 'android',
